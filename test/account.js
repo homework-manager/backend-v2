01ea21db610aa65772d7ea67b8210f683101e2e4
makmm <makmmelmejorenlacompu@gmail.com>
2018-05-05 20:48:21 -0300

    [test/account] edit account section + invalid username test

--- test/account.js
+++ test/account.js
@@ -14,77 +14,77 @@ describe('account', function () {
 
   describe('create account', function () {
 
-  it('should fail at creating account (no json)', () =>
-    fetch('/api/v1/account', { method: 'POST' })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(/IsRequired/.test(json.error), true);
-      })
-  );
+    it('should fail at creating account (no json)', () =>
+      fetch('/api/v1/account', { method: 'POST' })
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(/IsRequired/.test(json.error), true);
+        })
+    );
 
-  it('should fail at creating account (empty json)', () =>
-    fetch('/api/v1/account', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({})
-    })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(/IsRequired/.test(json.error), true);
+    it('should fail at creating account (empty json)', () =>
+      fetch('/api/v1/account', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
       })
-  );
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(/IsRequired/.test(json.error), true);
+        })
+    );
 
-  it('should fail at creating account (invalid username)', () =>
-    createAccount({ username: '(invalid username)' })
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'usernameIsInvalid');
-      })
-  );
+    it('should fail at creating account (invalid username)', () =>
+      createAccount({ username: '(invalid username)' })
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'usernameIsInvalid');
+        })
+    );
 
-  it('should fail at creating account (invalid password)', () =>
-    createAccount({ password: 'shrt' })
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'passwordIsInvalid');
-      })
-  );
+    it('should fail at creating account (invalid password)', () =>
+      createAccount({ password: 'shrt' })
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'passwordIsInvalid');
+        })
+    );
 
-  it('should fail at creating account (invalid email)', () =>
-    createAccount({ email: 'not valid email' })
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'emailIsInvalid');
-      })
-  );
+    it('should fail at creating account (invalid email)', () =>
+      createAccount({ email: 'not valid email' })
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'emailIsInvalid');
+        })
+    );
 
-  it('should fail at creating account (invalid full name)', () =>
-    createAccount({
-      fullname: Array(50).fill('bullshit').join('')
-    })
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'fullnameIsInvalid');
+    it('should fail at creating account (invalid full name)', () =>
+      createAccount({
+        fullname: Array(50).fill('bullshit').join('')
       })
-  );
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'fullnameIsInvalid');
+        })
+    );
 
-  it('should create a account', () =>
-    createAccount()
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, true);
-      })
-  );
+    it('should create a account', () =>
+      createAccount()
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, true);
+        })
+    );
 
   });
   
@@ -92,46 +92,61 @@ describe('account', function () {
   //  edit account
   // ################
 
-  it('should fail at editing account (not logged in)', () =>
-    fetch('/api/v1/account', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(getMockData())
-    })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'unauthorized');
-      })
-  );
+  describe('edit account', function () {
 
-  it('should fail at editing account (no json)', () =>
-    fetchWithToken('/api/v1/account', { method: 'PATCH' })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(/IsRequired/.test(json.error), true);
+    it('should fail at editing account (not logged in)', () =>
+      fetch('/api/v1/account', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(getMockData())
       })
-  );
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'unauthorized');
+        })
+    );
 
-  it('should fail at editing account (empty json)', () =>
-    fetchWithToken('/api/v1/account', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({})
-    })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(/IsRequired/.test(json.error), true);
+    it('should fail at editing account (no json)', () =>
+      fetchWithToken('/api/v1/account', { method: 'PATCH' })
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(/IsRequired/.test(json.error), true);
+        })
+    );
+
+    it('should fail at editing account (empty json)', () =>
+      fetchWithToken('/api/v1/account', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
       })
-  );
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(/IsRequired/.test(json.error), true);
+        })
+    );
+
+    it('should fail at editing account (invalid username)', () => 
+      editAccount({
+        username: '(invalid username)'
+      })
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'usernameIsInvalid');
+        })
+    );
+
+  });
 
 });
