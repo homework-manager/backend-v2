b43a1b6effeea05238a8c8012eba0b505500af1f
makmm <makmmelmejorenlacompu@gmail.com>
2018-05-06 11:35:29 -0300

    [test/group] make create group section

--- test/group.js
+++ test/group.js
@@ -8,48 +8,52 @@ const {
 
 describe('group', function () {
 
-  it('should fail at creating group (no json)', () =>
-    fetchWithToken('/api/v1/group', { method: 'POST' })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(/IsRequired/.test(json.error), true);
-      })
-  );
+  describe('create group', function () {
 
-  it('should fail at creating group (empty json)', () =>
-    fetchWithToken('/api/v1/group', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({})
-    })
-      .then(res => res.json())
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(/IsRequired/.test(json.error), true);
-      })
-  );
+    it('should fail at creating group (no json)', () =>
+      fetchWithToken('/api/v1/group', { method: 'POST' })
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(/IsRequired/.test(json.error), true);
+        })
+    );
 
-  it('should fail at creating group (invalid name)', () =>
-    createGroup({ name: Array(50).fill('bullshit').join('') })
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'nameIsInvalid');
+    it('should fail at creating group (empty json)', () =>
+      fetchWithToken('/api/v1/group', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
       })
-  );
+        .then(res => res.json())
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(/IsRequired/.test(json.error), true);
+        })
+    );
 
-  it('should fail at creating group (invalid joinName)', () =>
-    createGroup({ joinName: Array(50).fill('bullshit').join('') })
-      .then(json => {
-        assert.strictEqual(typeof json, 'object');
-        assert.strictEqual(json.success, false);
-        assert.strictEqual(json.error, 'joinNameIsInvalid');
-      })
-  );
+    it('should fail at creating group (invalid name)', () =>
+      createGroup({ name: Array(50).fill('bullshit').join('') })
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'nameIsInvalid');
+        })
+    );
+
+    it('should fail at creating group (invalid joinName)', () =>
+      createGroup({ joinName: Array(50).fill('bullshit').join('') })
+        .then(json => {
+          assert.strictEqual(typeof json, 'object');
+          assert.strictEqual(json.success, false);
+          assert.strictEqual(json.error, 'joinNameIsInvalid');
+        })
+    );
+
+  });
 
 });
